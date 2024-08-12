0b0c37fcae7c299dd4257a18deda7278bdca0ccc
Sandipan Mahata <Sandipan.m@muvin.in>
2024-08-12 22:13:01 +0530

    initialized

---
+++ 02_math.js
@@ -0,0 +1,15 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = add;
+// module.exports = { add, sub };
+
+// another way of doing this
+exports.add = (a, b) => a + b;
+
+exports.sub = (a, b) => a - b;
---
+++ 02_modules.js
@@ -0,0 +1,6 @@
+const math = require("./02_math");
+// const {add, sub} = require("./02_math");
+
+console.log(math);
+console.log(math.add(1, 2));
+console.log(math.sub(1, 2));
